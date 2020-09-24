7e9535cd67c523b6eaf7b1423dc0edbf882a82e5
Hamza BENBRAHIM <benbrahim.hamza@gmail.com>
2020-09-24 11:43:09 +0200

    Created the experience entity

---
+++ src/Entity/Experience.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ExperienceRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Different experiences, taken from different sources, mainly LinkedIn for now
+ * 
+ * @ApiResource
+ * @ORM\Entity(repositoryClass=ExperienceRepository::class)
+ */
+class Experience
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $startingDate;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $endingDate;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStartingDate(): ?\DateTimeInterface
+    {
+        return $this->startingDate;
+    }
+
+    public function setStartingDate(\DateTimeInterface $startingDate): self
+    {
+        $this->startingDate = $startingDate;
+
+        return $this;
+    }
+
+    public function getEndingDate(): ?\DateTimeInterface
+    {
+        return $this->endingDate;
+    }
+
+    public function setEndingDate(\DateTimeInterface $endingDate): self
+    {
+        $this->endingDate = $endingDate;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    // Custom init constructor
+    public function ____construct($name, $startingDate, $endingDate, $description) {
+        $this->name = $name;
+        $this->startingDate = $startingDate;
+        $this->endingDate = $endingDate;
+        $this->description = $description;
+    }
+}
